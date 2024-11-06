28e2b09cc497998de246b3a2ba1882be08cbeadb
ana-says-hi <lujerdeanana@yahoo.com>
2024-11-06 21:12:53 +0200

    adaugat clasele pt project activity

---
+++ Melodify_app/app/src/main/java/com/example/melodify_app/Model_Auxiliare/AudioFile.java
@@ -0,0 +1,6 @@
+package com.example.melodify_app.Model_Auxiliare;
+
+public class AudioFile {
+    String file_name;
+    String hashtag; //instrument
+}
---
+++ Melodify_app/app/src/main/java/com/example/melodify_app/Model_Auxiliare/Project.java
@@ -0,0 +1,9 @@
+package com.example.melodify_app.Model_Auxiliare;
+
+import java.util.ArrayList;
+
+public class Project {
+    String name;
+    ArrayList<String> textFiles;
+    ArrayList<AudioFile> audioFiles;
+}
