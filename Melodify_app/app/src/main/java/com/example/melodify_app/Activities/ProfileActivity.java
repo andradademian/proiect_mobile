12960864227dff11583ff8426f30923f078d8a17
ana-says-hi <lujerdeanana@yahoo.com>
2024-11-06 14:38:53 +0200

    no  biggie

--- Melodify_app/app/src/main/java/com/example/melodify_app/Activities/ProfileActivity.java
+++ Melodify_app/app/src/main/java/com/example/melodify_app/Activities/ProfileActivity.java
@@ -5,9 +5,12 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 
+import com.example.melodify_app.R;
+
 public class ProfileActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_layout);
     }
 }
