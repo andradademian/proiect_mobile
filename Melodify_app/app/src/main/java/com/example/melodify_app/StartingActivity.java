aea92a0061b1237a5b95a857d8abccef0574cca1
ana-says-hi <lujerdeanana@yahoo.com>
2024-11-06 11:47:22 +0200

    mere yay

---
+++ Melodify_app/app/src/main/java/com/example/melodify_app/StartingActivity.java
@@ -0,0 +1,26 @@
+package com.example.melodify_app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.annotation.Nullable;
+
+public class StartingActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.starting_layout);
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(StartingActivity.this, SignInActivity.class);
+                startActivity(intent);
+                finish(); // Close Starting activity so it's not in back stack
+            }
+        }, 2000);
+    }
+}
