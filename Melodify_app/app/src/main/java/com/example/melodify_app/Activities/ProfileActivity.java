4d0c657f68674e0eb543622bd81600db53d38a17
ana-says-hi <lujerdeanana@yahoo.com>
2024-11-07 00:05:26 +0200

    bug still there, dar avem titlu si motto in app

--- Melodify_app/app/src/main/java/com/example/melodify_app/Activities/ProfileActivity.java
+++ Melodify_app/app/src/main/java/com/example/melodify_app/Activities/ProfileActivity.java
@@ -25,12 +25,12 @@ public class ProfileActivity extends Activity {
         cardDataList.add(new ProjectCard("Title 1", "Description for card 1"));
         cardDataList.add(new ProjectCard("Title 2", "Description for card 2"));
         cardDataList.add(new ProjectCard("Title 3", "Description for card 3"));
-//        cardDataList.add(new ProjectCard("Title 4", "Description for card 4"));
-//        cardDataList.add(new ProjectCard("Title 5", "Description for card 5"));
-//        cardDataList.add(new ProjectCard("Title 6", "Description for card 6"));
-//        cardDataList.add(new ProjectCard("Title 7", "Description for card 7"));
-//        cardDataList.add(new ProjectCard("Title 8", "Description for card 8"));
-//        cardDataList.add(new ProjectCard("Title 9", "Description for card 9"));
+        cardDataList.add(new ProjectCard("Title 4", "Description for card 4"));
+        cardDataList.add(new ProjectCard("Title 5", "Description for card 5"));
+        cardDataList.add(new ProjectCard("Title 6", "Description for card 6"));
+        cardDataList.add(new ProjectCard("Title 7", "Description for card 7"));
+        cardDataList.add(new ProjectCard("Title 8", "Description for card 8"));
+        cardDataList.add(new ProjectCard("Title 9", "Description for card 9"));
 
         // Set up RecyclerView
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
