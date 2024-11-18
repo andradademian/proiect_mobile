362f68fb77bbea355e9e9ddab8e3b18bb6b2002b
Andrada <andradademian@gmail.com>
2024-11-18 15:11:30 +0200

    added project id

--- Melodify_app/app/src/main/java/com/example/melodify_app/Model_Auxiliare/Project.java
+++ Melodify_app/app/src/main/java/com/example/melodify_app/Model_Auxiliare/Project.java
@@ -3,6 +3,7 @@ package com.example.melodify_app.Model_Auxiliare;
 import java.util.ArrayList;
 
 public class Project {
+    String projectID;
     String name;
     String description;
     ArrayList<String> textFiles;
